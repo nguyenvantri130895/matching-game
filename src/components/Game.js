477d8f6faa5e90ecb993be0d7f66e3c2bfb8cf74
nguyenvantri130895 <nguyenvantri130895@gmail.com>
2018-11-08 17:07:18 +0700

    Update Game.js

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-//import { Link } from 'react-router-dom'
 import '../App.scss'
-//import { Link } from 'react-router-dom'
 import Card from './Card'
 
 class Game extends Component {
